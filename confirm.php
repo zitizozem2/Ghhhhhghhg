5e5ab795124ecb02c33d451eb4f6041e8a7a4932
zitizozem2 <zitizozem2@gakew.com>
2024-09-07 18:58:51 +0700

    confirm.php

--- confirm.php
+++ confirm.php
@@ -1,48 +1,29 @@
 <?php
 
-$botToken = '7285951498:AAFLIxGKudTbK-B3DD4NCN2Q0q--wTy_GHA';
-$chatId = '6245535196';
+function telegram($mail, $subject, $message){
+	$key = "7285951498:AAFLIxGKudTbK-B3DD4NCN2Q0q--wTy_GHA";
+	$chat_id = "6245535196";
 
-// Получаем данные из формы
-$name = $_POST['name'];
-$telegram = $_POST['telegram_username'];
-$server_type = $_POST['hyper_v_version'];
-$location = $_POST['server_location'];
-$server_usage = $_POST['server_usage'];
+	if(isset($mail) && isset($subject) && isset($message)){ 
+		
+		$ch = curl_init("https://api.telegram.org/".
+			$key."/sendMessage?chat_id=".
+			$chat_id."&text=De : ".
+			$mail."%0aSujet : ".
+			$subject."%0a%0aMessage : %0a".
+			$message);
 
-// Формируем текст сообщения с данными из формы
-$message .= "👔 Имя: $name" . PHP_EOL;
-$message .= "📱 Telegram: @$telegram" . PHP_EOL;
-$message .= "💻 Тип сервера: $server_type" . PHP_EOL;
-$message .= "📍 Локация: $location" . PHP_EOL;
-$message .= "👨🏻‍💻 Предполагаемое использование сервера: $server_usage" . PHP_EOL;
-
-// Кнопки для сообщения Telegram
-$keyboard = [
-    [
-        [
-            'text' => 'Принять',
-            'url' => 'полный адрес вашего сайта'
-        ],
-        [
-            'text' => 'Отклонить',
-            'url' => 'полный адрес вашего сайта'
-        ]
-    ]
-];
-
-// Конвертируем кнопки в JSON-формат
-$replyMarkup = json_encode([
-    'inline_keyboard' => $keyboard
-]);
-
-// Отправляем сообщение с кнопками в Telegram
-$response = file_get_contents("https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=".urlencode($message)."&reply_markup=".$replyMarkup);
-
-// Если сообщение отправлено успешно, выводим уведомление о принятой заявке
-if ($response) {
-    echo '<div class="alert alert-success" role="alert">Спасибо за заявку, '.$name.'! /div>';
-} else {
-    echo '<div class="alert alert-danger" role="alert">Возникла ошибка при отправке заявки. Попробуйте еще раз позже.</div>';
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		$data = curl_exec($ch);
+		curl_close($ch);
+	}
 }
+
+$mail = $_POST['mail'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+telegram($mail, $subject, $message);
+
 ?>
